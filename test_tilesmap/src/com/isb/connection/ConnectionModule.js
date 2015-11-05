9d4911554a8b1230c271e951fa3fdd55e7acddca
Mihai Corcosa <corcosa.mihai@isoftbet.com>
2015-11-05 13:26:10 +0200

    update Connection Module, now working

--- test_tilesmap/src/com/isb/connection/ConnectionModule.js
+++ test_tilesmap/src/com/isb/connection/ConnectionModule.js
@@ -1,65 +1,87 @@
 /**
  * Created by user60 on 28/10/15.
  */
+/// <reference path="../events/SignalsManager.ts" />
 var ConnectionModule;
 (function (ConnectionModule) {
     var Connection = (function () {
-        function Connection(grid, websocket) {
-            this.grid = grid;
+        function Connection(websocket) {
+            this.messagesToDispatch = [
+                "opponent_character",
+                "move",
+                "start_match",
+                "reconciliation",
+                "win",
+                "opponent_disconnected",
+                "move_time_expired",
+                "opponent_skip",
+                "error"
+            ];
             this.websocket = websocket;
-            this.signal = new Phaser.Signal;
             this.init();
         }
         Connection.prototype.init = function () {
-            //add listeners
-            //LISTENER FOR TESTING PURPOSES ONLY!
-            /*this.grid.signal.add(
-                function()
-                {
-                    console.log("Connection module got signal");
-                    this.sendSkip();
-                }
-            )*/
-            this.websocket.onclose = function (evt) {
-                this.onClose(evt);
-            };
+            //register signals
+            EventsModule.SignalsManager.getInstance().createBinding("find_match", this.findMatch, this);
+            EventsModule.SignalsManager.getInstance().createBinding("select_character", this.selectCharacter, this);
+            EventsModule.SignalsManager.getInstance().createBinding("move", this.sendMove, this);
+            EventsModule.SignalsManager.getInstance().createBinding("move_skip", this.skipMove, this);
+            this.websocket.cmodule = this;
             this.websocket.onmessage = function (evt) {
-                this.onMessage(evt);
+                this.cmodule.onMessage(evt);
             };
             this.websocket.onerror = function (evt) {
-                this.onError(evt);
+                this.cmodule.onError(evt);
             };
         };
         Connection.prototype.onMessage = function (evt) {
-            console.log("Received from server: " + evt.data);
+            var data = JSON.parse(evt.data);
+            console.log("FROM SERVER: " + data.name + " WITH PARAMS: " + data.param);
+            if (this.isInArray(data.name, this.messagesToDispatch))
+                EventsModule.SignalsManager.getInstance().dispatch(data.name, data.param);
         };
-        Connection.prototype.sendCharacterSelection = function (msg) {
-            doSendObj({
+        Connection.prototype.onError = function (evt) {
+            console.log("Connection error: " + evt.data);
+        };
+        Connection.prototype.findMatch = function (msg) {
+            this.doSendObj({
+                name: 'find_match',
+                param: msg
+            });
+        };
+        Connection.prototype.selectCharacter = function (msg) {
+            this.doSendObj({
                 name: 'select character',
                 param: msg
             });
         };
         Connection.prototype.sendMove = function (move, state) {
-            doSendObj({
+            this.doSendObj({
                 name: 'move',
                 param: move //TODO: send the correct param (move and state)
             });
         };
-        Connection.prototype.sendSkip = function () {
-            doSendObj({
+        Connection.prototype.skipMove = function () {
+            this.doSendObj({
                 name: 'move_skip',
                 param: null //TODO: send state (maybe)
             });
         };
         Connection.prototype.doSend = function (obj) {
-            console.log("SENT: " + message);
-            websocket.send(message);
+            console.log("SENT: " + obj);
+            this.websocket.send(obj);
         };
         Connection.prototype.doSendObj = function (obj) {
-            doSend(JSON.stringify(obj));
+            this.doSend(JSON.stringify(obj));
         };
-        Connection.prototype.terminate = function () {
-            websocket.close();
+        Connection.prototype.isInArray = function (value, array) {
+            var count = array.length;
+            for (var i = 0; i < count; i++) {
+                if (array[i] === value) {
+                    return true;
+                }
+            }
+            return false;
         };
         return Connection;
     })();
--- test_tilesmap/src/com/isb/connection/ConnectionModule.ts
+++ test_tilesmap/src/com/isb/connection/ConnectionModule.ts
@@ -1,18 +1,39 @@
 /**
  * Created by user60 on 28/10/15.
  */
+/// <reference path="../events/SignalsManager.ts" />
 
 module ConnectionModule{
     export class Connection{
         websocket;
+        messagesToDispatch;
+        const FIND_MATCH: string = "find_match";
+        const SELECT_CHARACTER: string = "select_character";
+        const MOVE: string = "move";
+        const MOVE_SKIP: string = "move_skip";
+
 
         constructor(websocket){
+            this.messagesToDispatch = [
+                "opponent_character",
+                "move",
+                "start_match",
+                "reconciliation",
+                "win",
+                "opponent_disconnected",
+                "move_time_expired",
+                "opponent_skip",
+                "error"];
             this.websocket = websocket;
             this.init();
         }
 
         init(){
-            //add listeners
+            //register signals
+            EventsModule.SignalsManager.getInstance().createBinding(this.FIND_MATCH, this.findMatch, this);
+            EventsModule.SignalsManager.getInstance().createBinding(this.SELECT_CHARACTER, this.selectCharacter, this);
+            EventsModule.SignalsManager.getInstance().createBinding(this.MOVE, this.sendMove, this);
+            EventsModule.SignalsManager.getInstance().createBinding(this.MOVE_SKIP, this.skipMove, this);
 
             this.websocket.cmodule = this;
             this.websocket.onmessage = function(evt) { this.cmodule.onMessage(evt) };
@@ -20,10 +41,26 @@ module ConnectionModule{
         }
 
         onMessage(evt){
-            EventsModule.SignalsManager.getInstance().dispatch("test", tile);
+            var data = JSON.parse(evt.data);
+            console.log("FROM SERVER: "+data.name+" WITH PARAMS: "+data.param);
+
+            //TODO: handle the issue with 'move' coming from both internal and server
+            if(this.isInArray(data.name, this.messagesToDispatch))
+                EventsModule.SignalsManager.getInstance().dispatch(data.name, data.param);
         }
 
-        sendCharacterSelection(msg){
+        onError(evt){
+            console.log("Connection error: "+evt.data)
+        }
+
+        findMatch(msg){
+            this.doSendObj({
+                name: 'find_match',
+                param: msg
+            });
+        }
+
+        selectCharacter(msg){
             this.doSendObj({
                 name: 'select character',
                 param: msg
@@ -37,7 +74,7 @@ module ConnectionModule{
             });
         }
 
-        sendSkip(){
+        skipMove(){
             this.doSendObj({
                 name: 'move_skip',
                 param: null //TODO: send state (maybe)
@@ -45,7 +82,7 @@ module ConnectionModule{
         }
 
         doSend(obj){
-            console.log("SENT: " + obj);
+            console.log("SENT TO SERVER: " + obj);
             this.websocket.send(obj);
         }
 
@@ -53,8 +90,14 @@ module ConnectionModule{
             this.doSend(JSON.stringify(obj));
         }
 
-        terminate(){
-            this.websocket.close();
+        isInArray(value, array) {
+            var count=array.length;
+            for(var i=0;i<count;i++)
+            {
+                if(array[i]===value){return true;}
+            }
+            return false;
         }
+
     }
 }
