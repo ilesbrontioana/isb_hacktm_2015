94011ffc3e07f343a27c328a0c5094545c29c922
Popescu Alexandru <aleximodev@gmail.com>
2015-11-05 23:30:43 +0200

    MVC example

--- spudz/app.ts
+++ spudz/app.ts
@@ -2,12 +2,15 @@
  * Created by adm on 12.10.15.
  */
 /// <reference path="phaser.d.ts" />
+/// <reference path="com/MVC/MVC.ts" />
 /// <reference path="com/managers/events/SignalsManager.ts" />
 /// <reference path="com/grid/Grid.ts" />
 /// <reference path="com/managers/game/GameController.ts" />
 /// <reference path="com/managers/map/MapLoader.ts" />
 /// <reference path="com/managers/sounds/SoundsManager.ts" />
 /// <reference path="com/actors/Character.ts" />
+/// <reference path="com/ui/UI.ts" />
+/// <reference path="com/managers/graphics/GraphicsManager.ts" />
 
 class SimpleGame {
     game: Phaser.Game;
@@ -15,23 +18,26 @@ class SimpleGame {
     map:MapModule.Map;
     character:CharacterModule.Character;
     background;
+    hud;
 
     constructor() {
-        this.game = new Phaser.Game(1024, 768, Phaser.AUTO, 'content', {
+        this.game = new Phaser.Game(1336, 740, Phaser.AUTO, 'content', {
             create: this.create,
             preload: this.preload,
             render: this.render,
             update: this.update,
-            //onTest1:this.onTest1,
+            onTestMVC:this.onTestMVC,
             onTest2:this.onTest2
         });
     }
 
     preload() {
+        this.onTestMVC();
 
         GameControllerModule.GameController.getInstance().game = this.game;
+        GraphicsModule.GraphicsManager.getInstance().loadAtlas("ui", "../../spudz/bin/assets/ui/", 'UI SpriteSheet.png', 'UI SpriteSheet.json');
 
-        this.game.load.image('bg', '../../spudz/bin/assets/background/bg1.jpg');
+        this.game.load.image('bg', '../../spudz/bin/assets/background/Background3.jpg');
 
         this.character = new CharacterModule.Character();
         this.character.load('phaser-dude');
@@ -73,10 +79,17 @@ class SimpleGame {
 
         SoundsModule.SoundsManager.getInstance().playSound('sound3');
 
+        this.hud = new UserInterfaceModule.hud();
     }
 
-    onTest1(tile) {
-        console.log("ON TEST 1    "+this+" !?!?!?!?! "+test);
+    onTestMVC() {
+        var startUpCommand:MvcModule.Controller = new MvcModule.Controller();
+        MvcModule.Mvc.getInstance().registerCommand("Startup", startUpCommand);
+
+        MvcModule.Mvc.getInstance().registerMediator("TestMediator",new testMediator(new testView()));
+        MvcModule.Mvc.getInstance().registerProxy("TestProxy", new testProxy());
+
+        MvcModule.Mvc.getInstance().sendNotification("Startup",1, "test");
     }
 
     onTest2(tile) {
@@ -88,4 +101,31 @@ class SimpleGame {
 window.onload = () => {
     var game = new SimpleGame();
 };
+
+//////////////////////////////////////////////////////
+class testView extends MvcModule.View{
+    constructor(){
+        console.log("za test view")
+    }
+}
+
+class testMediator extends MvcModule.Mediator{
+    constructor(viewComponent:MvcModule.View){
+        super(viewComponent);
+    }
+
+    listNotificationInterests():Array{
+        return ["Startup", "TestProxy"];
+    }
+
+    handleNotification(notification:MvcModule.INotification){
+        console.log(notification.name+" "+notification.body);
+    }
+}
+
+class testProxy extends MvcModule.Proxy{
+    constructor(){
+        this.sendNotification("TestProxy", 2)
+    }
+}
 
