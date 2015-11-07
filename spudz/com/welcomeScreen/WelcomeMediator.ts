0107349d0ec12f2258bb34ca514251a92b8d6eb9
Popescu Alexandru <aleximodev@gmail.com>
2015-11-07 19:59:54 +0200

    Stuff

---
+++ spudz/com/welcomeScreen/WelcomeMediator.ts
@@ -0,0 +1,35 @@
+/**
+ * Created by adm on 07.11.15.
+ */
+module WelcomeModule{
+    export class WelcomeMediator extends MvcModule.Mediator{
+        static NAME:string = this+"WelcomeMediator";
+
+        constructor(viewComponent:MvcModule.View){
+            super(viewComponent);
+            this.addListenerToSignal("StartGame", function(){
+                MvcModule.Mvc.getInstance().registerMediator(GridModule.GridMediator.NAME, new GridModule.GridMediator(new GridModule.GridView()));
+
+                MvcModule.Mvc.getInstance().registerMediator(CharacterModule.CharacterMediator.NAME, new CharacterModule.CharacterMediator(new CharacterModule.CharacterView()));
+                MvcModule.Mvc.getInstance().registerProxy(CharacterModule.CharacterProxy.NAME, new CharacterModule.CharacterProxy());
+
+                MvcModule.Mvc.getInstance().registerMediator(UserInterfaceModule.UIMediator.NAME, new UserInterfaceModule.UIMediator(new UserInterfaceModule.UIView()));
+                MvcModule.Mvc.getInstance().sendNotification(UserInterfaceModule.UINotifications.HIDE_ACTIONS_MENU);
+
+                MvcModule.Mvc.getInstance().registerCommand(RoundsModule.RoundsCommand.NAME, new RoundsModule.RoundsCommand());
+                MvcModule.Mvc.getInstance().registerMediator(RoundsModule.RoundsMediator.NAME, new RoundsModule.RoundsMediator(new RoundsModule.RoundsView()));
+                MvcModule.Mvc.getInstance().registerProxy(RoundsModule.RoundsProxy.NAME, new RoundsModule.RoundsProxy());
+            });
+        }
+
+        listNotificationInterests():Array{
+            return [WelcomeModule.WelcomeNotifications.WELCOME];
+        }
+
+        handleNotification(notification:MvcModule.INotification) {
+            switch (notification.name) {
+
+            }
+        }
+    }
+}
---
+++ spudz/com/welcomeScreen/WelcomeNotifications.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by adm on 07.11.15.
+ */
+module WelcomeModule{
+    export class WelcomeNotifications{
+        static WELCOME:string = "welcome";
+    }
+}
