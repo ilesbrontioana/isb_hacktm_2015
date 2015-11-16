2c144d3d743fbb5212948ab966d41f81a0b4ae15
ilesbrontioana <iles.bront.ioana@gmail.com>
2015-11-16 14:23:13 +0200

    Action ray fix

--- spudz/com/actors/ActionRayView.ts
+++ spudz/com/actors/ActionRayView.ts
@@ -57,14 +57,16 @@ module CharacterModule
             this.moveCircle  = this.game.add.sprite(0, 0,
                                 this.game.cache.getBitmapData("GreenCircleBMP"));
             this.moveCircle.inputEnabled = true;
-            this.moveCircle.alpha = 0;
+            this.moveCircle.alpha = 0.5;
+            this.moveCircle.visible = false;
             this.moveCircle.anchor.setTo(0.5, 0.5);
             this.moveCircle.events.onInputDown.add(this.circleTouched, this);
 
             this.attackCircle  = this.game.add.sprite(0, 0,
                 this.game.cache.getBitmapData("RedCircleBMP"));
             this.attackCircle.inputEnabled = true;
-            this.attackCircle.alpha = 0;
+            this.attackCircle.alpha = 0.5;
+            this.attackCircle.visible = false;
             this.attackCircle.anchor.setTo(0.5, 0.5);
             this.attackCircle.events.onInputDown.add(this.circleTouched, this);
             this.game.physics.enable(this.attackCircle, Phaser.Physics.ARCADE);
@@ -89,30 +91,27 @@ module CharacterModule
 
             this.currentCircle.x = x;
             this.currentCircle.y = y;
-            this.currentCircle.alpha = 0.5;
+            this.currentCircle.visible = true;
 
         }
 
         removeActionRay()
         {
-            if(this.attackCircle.alpha > 0)
+            if(this.attackCircle.visible == true)
             {
-                this.attackCircle.alpha = 0;
+                this.attackCircle.visible = false;
             }
 
-            if(this.moveCircle.alpha > 0)
+            if(this.moveCircle.visible == true)
             {
-                this.moveCircle.alpha = 0;
+                this.moveCircle.visible = false;
             }
         }
 
         circleTouched(circle:Phaser.Sprite, pointer:Phaser.Pointer){
-            if(circle.alpha > 0)
-            {
-                var tile = this.getTileAt(pointer.worldX, pointer.worldY);
-                this.signalsManager.dispatch("TiledClicked", tile.graphics);
-                this.removeActionRay();
-            }
+            var tile = this.getTileAt(pointer.worldX, pointer.worldY);
+            this.signalsManager.dispatch("TiledClicked", tile.graphics);
+            this.removeActionRay();
         }
 
         getTileAt(x:number, y:number):GridModule.Tile
